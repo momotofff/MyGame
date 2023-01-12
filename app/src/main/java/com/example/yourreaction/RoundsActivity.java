24985f022ebe59ea1480f0b93ae10082a46ccc7d
I <momotofff@mail.ru>
2023-01-12 14:36:49 +0400

    My new project

---
+++ app/src/main/java/com/example/yourreaction/RoundsActivity.java
@@ -0,0 +1,13 @@
+package com.example.yourreaction;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoundsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rounds);
+    }
+}
