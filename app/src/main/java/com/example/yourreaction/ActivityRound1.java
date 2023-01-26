000dc571c406091512d12eab61e6886da05d3c78
I <momotofff@mail.ru>
2023-01-26 16:44:24 +0400

    чуть чуть даже работает

--- app/src/main/java/com/example/yourreaction/ActivityRound1.java
+++ app/src/main/java/com/example/yourreaction/ActivityRound1.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 public class ActivityRound1 extends AppCompatActivity
@@ -16,6 +18,7 @@ public class ActivityRound1 extends AppCompatActivity
     static ArrayList<View> textViews;
     static TimeCounter timeCounter;
     static int[] timeSleep;
+    TextView res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,25 +26,31 @@ public class ActivityRound1 extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_round1);
 
+        res = findViewById(R.id.res0);
+
         Intent intent = getIntent();
         timeSleep = intent.getIntArrayExtra("name");
         textViews = findViewById(R.id.results).getTouchables();
         buttonStart = findViewById(R.id.buttonStart);
         tap = findViewById(R.id.buttonTap);
         timeCounter = new TimeCounter();
-        tap.setClickable(false);
-        
+
         buttonStart.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
+                tap.setVisibility(View.INVISIBLE);
                 buttonStart.setClickable(false);
+                TimeCounter.start();
 
-                for (int i = 0; i < textViews.size(); ++i)
+                for (int i = 0; i < timeSleep.length; ++i)
                 {
-                    tap.setVisibility(View.INVISIBLE);
-
-                    try {Thread.sleep(timeSleep[i]);}
+                    try
+                    {
+                        Thread.sleep(timeSleep[i]);
+                        tap.setVisibility(View.VISIBLE);
+                    }
                     catch (InterruptedException e) {e.printStackTrace();}
                 }
             }
@@ -49,28 +58,14 @@ public class ActivityRound1 extends AppCompatActivity
 
         tap.setOnClickListener(new View.OnClickListener()
         {
+            @SuppressLint("SetTextI18n")
             @Override
-            public void onClick(View view) {
-                for (int i = 0; i < textViews.size(); ++i) {
-                    tap.setVisibility(View.INVISIBLE);
-                    try {
-                        Thread.sleep(timeSleep[i]);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    tap.setClickable(true);
-                    tap.setVisibility(Button.VISIBLE);
-                    TimeCounter.start();
-                    //tap(i);
+            public void onClick(View view)
+            {
+                synchronized (res) {
+                    res.setText(Long.toString(TimeCounter.getLeadTime()));
                 }
             }
         });
-
-    //public void tap(int i)
-    //{
-        //View view = new View(textViews.get(i).getContext());
-        //textViews.add(i, view);
-    //}
     }
 }
