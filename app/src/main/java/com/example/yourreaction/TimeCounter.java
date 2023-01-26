c0290985bab25926e1e73e11e57104a6865cf803
Momotofff <momotofff@mail.ru>
2023-01-26 22:33:07 +0400

    в общем ебучий интерфейс андроида выебал все мозги. кнопки нажимаются вьюшки пишутся тудушки оставлены

--- app/src/main/java/com/example/yourreaction/TimeCounter.java
+++ app/src/main/java/com/example/yourreaction/TimeCounter.java
@@ -5,9 +5,7 @@ public class TimeCounter
     long start;
     long end;
 
-    public void start() {
-        start = System.currentTimeMillis();
-    }
+    public void start() { start = System.currentTimeMillis();}
     public void finish() { end = System.currentTimeMillis(); }
     public long getLeadTime() { return end - start; }
     public void reset() { start = 0; end = 0; }
