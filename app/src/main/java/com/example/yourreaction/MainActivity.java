40d7faad34ca58968f667b884d7147be3e7df57f
Ignition <the_unforg@mal.ru>
2023-03-04 22:55:10 +0400

    refactoring

--- app/src/main/java/com/example/yourreaction/MainActivity.java
+++ app/src/main/java/com/example/yourreaction/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
